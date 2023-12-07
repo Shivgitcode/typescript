52aef0f4e36623f4449120403740022cabb29ce6
shivansh <shivneeraj2004@gmail.com>
2023-12-07 18:20:29 +0530

    studying function

---
+++ function.js
@@ -0,0 +1,11 @@
+// function square(num: number) {
+//   return num * num;
+// }
+// function greet(person: string) {
+//   return `Hi there,${person}`;
+// }
+var doSomething = function (person, age) {
+    return "hi i am ".concat(person, " and my age is ").concat(age);
+};
+// square(3);
+// greet("shivansh");
---
+++ function.ts
@@ -0,0 +1,21 @@
+// function square(num: number) {
+//   return num * num;
+// }
+
+// function greet(person: string) {
+//   return `Hi there,${person}`;
+// }
+
+// const doSomething = (person: String, age: number) => {
+//   return `hi i am ${person} and my age is ${age}`;
+// };
+
+// square(3);
+// greet("shivansh");
+
+function asd(num: number = 10) {
+  return num;
+}
+
+asd(70);
+asd("fhdk");
